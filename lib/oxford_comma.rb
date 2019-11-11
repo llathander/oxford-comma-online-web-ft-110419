7d4f3f8c45951e03feaf8ffa5c24f03f20173fd6
Adrian Samaniego <llathander@gmail.com>
2019-11-11 17:53:49 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,5 +1,8 @@
 def oxford_comma(array)
   if array.length == 2
-    array.to_s
+    return "#{array[0]} and #{array[1]}"
+  elsif 2 < array.length
+    array[-1].insert(0, "and ")
   end
+  array.join(", ")    
 end
