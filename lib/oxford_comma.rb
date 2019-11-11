63290332974f08b6fa7e07f2c066a9367007acbc
Adrian Samaniego <llathander@gmail.com>
2019-11-11 18:01:38 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,8 +1,7 @@
 def oxford_comma(array)
   if array.length == 2
     return "#{array[0]} and #{array[1]}"
-  elsif
-    2 < array.length
+  elsif 2 < array.length
     array[-1].insert(0, "and ")
   end
 end
