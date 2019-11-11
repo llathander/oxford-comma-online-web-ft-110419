5c41e9b2888e90f61b0ece1cce486d2603757a02
Adrian Samaniego <llathander@gmail.com>
2019-11-11 17:53:58 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,8 +1,5 @@
 def oxford_comma(array)
   if array.length == 2
-    return "#{array[0]} and #{array[1]}"
-  elsif 2 < array.length
-    array[-1].insert(0, "and ")
+    array.to_s
   end
-  array.join(", ")    
 end
